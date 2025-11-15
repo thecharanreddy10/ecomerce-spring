a8187f751af487e6b934fb683b3dc1415c334af0
thecharanreddy10 <sricharanreddyreddy@gmail.com>
2025-11-15 15:51:00 +0530

    model- created product

---
+++ src/main/java/com/telusko/ecom_proj/model/Product.java
@@ -0,0 +1,28 @@
+package com.telusko.ecom_proj.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Product {
+    @Id
+    private int id;
+    private String name;
+    private String description;
+    private String brand;
+    private BigDecimal price;
+    private String category;
+    private Date releaseDate;
+    private Boolean available;
+    private int quantity;
+}
